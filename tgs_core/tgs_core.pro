96df513a2535be21304d528a17765bf026cbedb3
divby0 <divisionby0x00@gmail.com>
2016-03-06 21:04:08 +0100

    Call linked list from demo

--- tgs_core/tgs_core.pro
+++ tgs_core/tgs_core.pro
@@ -7,12 +7,14 @@ CONFIG += console debug staticlib
 QMAKE_CFLAGS = -pedantic -std=c99 -Wall
 
 SOURCES += src/memory.c \
+    src/list.c \
     src/logger.c \
     src/stringutils.c
 
 HEADERS += \
     src/memory.h \
     src/typealias.h \
+    src/list.h \
     src/logger.h \
     src/sizes.h \
     src/stringutils.h
--- tgs_demo/src/demo.c
+++ tgs_demo/src/demo.c
@@ -2,10 +2,34 @@
 #include "memory.h"
 #include "typealias.h"
 #include "logger.h"
+#include "list.h"
+
+void* string_init(void* params);
+void string_free(TGS_LIST_NODE* node);
 
 int main(int argc, char** argv) {
     TGS_LOGGER* logger = logger_init(NULL, LVL_INFO);
     logger->log(logger, "Testing message", LVL_INFO);
+
+    TGS_LLIST* list = list_init(&string_init, &string_free);
+    list->add(list, "String01");
+    list->add(list, "String02");
+    list->add(list, "String03");
+    list_view(list);
+    list_quit(list);
+
     logger_quit(logger);
+
     return 0;
 }
+
+void* string_init(void* params) {
+    char* buffer = (char*)memalloc(1024);
+    strcpy(buffer, params);
+    return buffer;
+}
+
+void string_free(TGS_LIST_NODE* node) {
+    memfree(node->object);
+}
+
